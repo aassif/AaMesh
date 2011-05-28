4fff4077f6ede8e52df9cdf110b1eaf0ce19b733
Aa! <aassif.benassarou@univ-reims.fr>
2011-05-28 13:07:40 +0200

    Ajout de l'export VRML.

--- include/bits/AaMeshExport.hh
+++ include/bits/AaMeshExport.hh
@@ -8,6 +8,11 @@ namespace Aa
 {
   namespace Mesh
   {
+
+////////////////////////////////////////////////////////////////////////////////
+// export_off //////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
     void export_off_vertex (std::ostream & o, const BasicVertex & v)
     {
       o << v.coords [0] << ' ' << v.coords [1] << ' ' << v.coords [2] << std::endl;
@@ -38,6 +43,54 @@ namespace Aa
 
       ofs.close ();
     }
+
+////////////////////////////////////////////////////////////////////////////////
+// export_off //////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+    void export_vrml_vertex (ostream & o, const BasicVertex & v)
+    {
+      o << "        " << v.coords [0] << ' ' << v.coords [1] << ' ' << v.coords [2] << endl;
+    }
+
+    void export_vrml_triangle (ostream & o, const BasicTriangle & t)
+    {
+      o << "      " << t.indices [0] << ", " << t.indices [1] << ", " << t.indices [2] << ", -1\n";
+    }
+
+    void export_vrml (const BasicMesh * mesh, const string & filename)
+    {
+      const vector<BasicVertex>   & vertices  = mesh->getVertices ();
+      const vector<BasicTriangle> & triangles = mesh->getTriangles ();
+
+      ofstream of (filename.c_str ());
+      of << "#VRML V2.0 utf8\n"
+         << "Shape {\n"
+         << "  appearance Appearance {\n"
+         << "    material Material {}\n"
+         << "    }\n"
+         << "  geometry IndexedFaceSet {\n"
+         << "    coord Coordinate {\n"
+         << "      point [\n";
+      if (! vertices.empty ())
+      {
+        export_vrml_vertex (of, vertices [0]);
+        for (int i = 1; i < vertices.size ();) export_vrml_vertex (of, vertices [i++]);
+      }
+      of << "      ]\n"
+         << "    }\n"
+         << "    coordIndex [\n";
+      if (! triangles.empty ())
+      {
+        export_vrml_triangle (of, triangles [0]);
+        for (int i = 1; i < triangles.size ();) export_vrml_triangle (of, triangles [i++]);
+      }
+      of << "    ]\n"
+         << "  }\n"
+         << "}\n";
+      of.close ();
+    }
+
   }
 }
 
