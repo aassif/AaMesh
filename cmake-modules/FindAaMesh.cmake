3b10d81e5f5364e1e65e7bf59f1a2059cdfb4a58
Romain <romain.guillemot@univ-reims.fr>
2013-03-25 13:38:01 +0100

    ajout du fichier de module pour cmake

---
+++ cmake-modules/FindAaMesh.cmake
@@ -0,0 +1,41 @@
+# Include.
+find_path(AAMESH_INCLUDE_DIR
+    NAMES
+	AaMesh
+    AaMeshExport
+    AaMeshGts
+    AaMeshNOFF
+    AaMeshOptimizer
+    AaMeshPolygonizer
+    AaMeshPrimitives
+    AaMeshRenderer
+    AaMeshSeparator
+    AaMeshVBO
+	bits/AaMesh.hh
+    bits/AaMeshBox.hh
+    bits/AaMeshExport.hh
+    bits/AaMeshGts.hh
+    bits/AaMeshNOFF.hh
+    bits/AaMeshOptimizer.hh
+    bits/AaMeshPolygonizer.hh
+    bits/AaMeshPrimitives.hh
+    bits/AaMeshRenderer.hh
+    bits/AaMeshSeparator.hh
+    bits/AaMeshVBO.hh
+    PATHS
+    ${AAMESH_PATH}/include
+)
+
+if(AAMESH_INCLUDE_DIR)
+    set(AAMESH_FOUND TRUE)
+endif(AAMESH_INCLUDE_DIR)
+
+if(AAMESH_FOUND)
+    if(NOT AAMESH_FIND_QUIETLY)
+        message(STATUS "Found AAMESH : ${AAMESH_DIR}")
+    endif(NOT AAMESH_FIND_QUIETLY)
+else(AAMESH_FOUND)
+    if(AAMESH_FIND_REQUIRED)
+        message(FATAL_ERROR "Could not find AAMESH")
+    endif(AAMESH_FIND_REQUIRED)
+endif(AAMESH_FOUND)
